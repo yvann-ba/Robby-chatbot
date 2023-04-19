320476ac687db4ea0e9051b4b25ef6904d3a014d
Gabriele Arcangelo Scalici <gaba41@gmail.com>
2023-04-19 04:26:00 +0200

    style: minor layout changes and add home link

--- modules/layout.py
+++ modules/layout.py
@@ -7,7 +7,10 @@ class Layout:
         Displays the header of the app
         """
         st.markdown(
-            "<h1 style='text-align: center;'>ChatBot-PDF, Talk with your documents ! 💬</h1>", unsafe_allow_html=True
+            """
+            <h1 style='text-align: center;'>ChatBot-PDF, Talk with your documents ! 💬</h1>
+            """,
+            unsafe_allow_html=True,
         )
 
     def show_api_key_error(self):
@@ -15,6 +18,10 @@ class Layout:
         Displays an error message if the user has not entered an API key
         """
         st.markdown(
-            "<div style='text-align: center;'><h4>Enter your OpenAI API key to start chatting 😉</h4></div>",
+            """
+            <div style='text-align: center;'>
+                <h4>Enter your <a href="https://platform.openai.com/account/api-keys" target="_blank">OpenAI API key</a> to start chatting 🤓</h4>
+            </div>
+            """,
             unsafe_allow_html=True,
         )
