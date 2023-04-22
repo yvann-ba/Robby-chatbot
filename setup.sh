666643fd1738f55ed8095374f7c35ea06d23400f
Yvann <yvannbarbotts@gmail.com>
2023-04-22 19:21:52 +0200

    Merge the branch for synchronize the server files

---
+++ setup.sh
@@ -0,0 +1,18 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"yyvannbarbotts@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+\n\
+[theme]\n\
+base = \"light\"\n\
+backgroundColor = \"#FFF1F9\"\n\
+secondaryBackgroundColor = \"#FFDCF1\"\n\
+" > ~/.streamlit/config.toml
