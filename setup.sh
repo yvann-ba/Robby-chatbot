8d237ae12be57934d5e2b6569bb86838618cff40
Yvann <97234242+yvann-hub@users.noreply.github.com>
2023-04-25 11:47:53 +0200

    debug issue #12 now possible to actualize change made to modules in localhost

--- setup.sh
+++ setup.sh
@@ -17,5 +17,5 @@ primaryColor = \"#89CFF0\"\n\
 backgroundColor = \"#E0F7FE\"\n\
 secondaryBackgroundColor = \"#FFFCE4\"\n\
 textColor = \"#000000\"\n\
-font = \"sans-serif\"\n\
+font = \"sans serif\"\n\
 " > ~/.streamlit/config.toml
