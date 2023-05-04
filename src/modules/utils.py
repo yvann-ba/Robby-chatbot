8563d26912b5d6a85f758fb4d50d4b5bd13ce065
Yvann <97234242+yvann-hub@users.noreply.github.com>
2023-05-04 20:38:29 +0200

    Now work for PDF files
    i need to change the name of the repo lol

--- src/modules/utils.py
+++ src/modules/utils.py
@@ -1,13 +1,16 @@
 import os
 import pandas as pd
 import streamlit as st
-
+import PyPDF2
+from io import BytesIO
 from modules.chatbot import Chatbot
 from modules.embedder import Embedder
 from langchain.callbacks import get_openai_callback
+import pdfplumber
 
 
 class Utilities:
+
     @staticmethod
     def load_api_key():
         """
@@ -24,30 +27,50 @@ class Utilities:
             if user_api_key:
                 st.sidebar.success("API key loaded", icon="🚀")
         return user_api_key
+    
 
+    
     @staticmethod
     def handle_upload():
         """
         Handles the file upload and displays the uploaded file
         """
-        uploaded_file = st.sidebar.file_uploader("upload", type="csv", label_visibility="collapsed")
+        uploaded_file = st.sidebar.file_uploader("upload", type=["csv", "pdf"], label_visibility="collapsed")
         if uploaded_file is not None:
-            
-
-            def show_user_file(uploaded_file):
+            def show_csv_file(uploaded_file):
                 file_container = st.expander("Your CSV file :")
-                shows = pd.read_csv(uploaded_file)
                 uploaded_file.seek(0)
+                shows = pd.read_csv(uploaded_file)
+                
                 file_container.write(shows)
-             
 
-            show_user_file(uploaded_file)
+            def show_pdf_file(uploaded_file):
+                
+                file_container = st.expander("Your PDF file :")
+                with pdfplumber.open(uploaded_file) as pdf:
+                    pdf_text = ""
+                    for page in pdf.pages:
+                        pdf_text += page.extract_text() + "\n"
+                file_container.write(pdf_text)
+            
+            def get_file_extension(uploaded_file):
+                return os.path.splitext(uploaded_file)[1].lower()
+            file_extension = get_file_extension(uploaded_file.name)
+
+
+            if file_extension == ".csv" :
+                show_csv_file(uploaded_file)
+            #elif file_extension== ".pdf" : 
+            #    show_pdf_file(uploaded_file)
+
         else:
             st.sidebar.info(
-                "👆 Upload your CSV file to get started, "
-                "sample for try : [fishfry-locations.csv](https://drive.google.com/file/d/1TpP3thVnTcDO1_lGSh99EKH2iF3GDE7_/view?usp=sharing)"
+                "👆 Upload your CSV or PDF file to get started, "
+                "CSV sample for try : [fishfry-locations.csv](https://drive.google.com/file/d/1TpP3thVnTcDO1_lGSh99EKH2iF3GDE7_/view?usp=sharing),"
             )
             st.session_state["reset_chat"] = True
+        #print(uploaded_file)
+        
         return uploaded_file
 
     @staticmethod
@@ -55,11 +78,12 @@ class Utilities:
         """
         Sets up the chatbot with the uploaded file, model, and temperature
         """
+        original_filename = uploaded_file
         embeds = Embedder()
         with st.spinner("Processing..."):
             uploaded_file.seek(0)
             file = uploaded_file.read()
-            vectors = embeds.getDocEmbeds(file, uploaded_file.name)
+            vectors = embeds.getDocEmbeds(file, original_filename.name)
             chatbot = Chatbot(model, temperature, vectors)
         st.session_state["ready"] = True
         return chatbot
