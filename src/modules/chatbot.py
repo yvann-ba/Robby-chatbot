c228d8b2677bba1219a0427e30fc935f193e8627
Yvann <97234242+yvann-hub@users.noreply.github.com>
2023-05-05 12:01:51 +0200

    Clean code and add comments

--- src/modules/chatbot.py
+++ src/modules/chatbot.py
@@ -4,60 +4,55 @@ from langchain.chains import ConversationalRetrievalChain
 from langchain.prompts.prompt import PromptTemplate
 from langchain.callbacks import get_openai_callback
 
-def count_tokens_chain(chain, query):
-    with get_openai_callback() as cb:
-        result = chain.run(query)
-        st.write(f'###### Tokens used in this conversation : {cb.total_tokens} tokens')
 
-        
-
-    return result 
 class Chatbot:
-    _template = """Given the following conversation and a follow-up question, rephrase the follow-up question to be a standalone question.
-    Chat History:
-    {chat_history}
-    Follow-up entry: {question}
-    Standalone question:"""
-
-    CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
-
-    qa_template = """"You are a friendly conversational assistant named Robby, designed to answer questions and chat with the user from a contextual file.
-    You receive data from a user's file and a question, you must help the user find the information they need. 
-    Your answers must be user-friendly and respond to the user in the language they speak to you.
-    question: {question}
-    =========
-    context: {context}
-    =======
-    """
-
-    QA_PROMPT = PromptTemplate(template=qa_template, input_variables=["question", "context"])
 
     def __init__(self, model_name, temperature, vectors):
         self.model_name = model_name
         self.temperature = temperature
         self.vectors = vectors
 
+    _template = """Given the following conversation and a follow-up question, rephrase the follow-up question to be a standalone question.
+        Chat History:
+        {chat_history}
+        Follow-up entry: {question}
+        Standalone question:"""
+    CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
 
+    qa_template = """You are a friendly conversational assistant named Robby, designed to answer questions and chat with the user from a contextual file.
+        You receive data from a user's file and a question, you must help the user find the information they need. 
+        Your answers must be user-friendly and respond to the user in the language they speak to you.
+        question: {question}
+        =========
+        context: {context}
+        ======="""
+    QA_PROMPT = PromptTemplate(template=qa_template, input_variables=["question", "context"])
 
     def conversational_chat(self, query):
-            """
-            Starts a conversational chat with a model via Langchain
-            """
-            llm = ChatOpenAI(model_name=self.model_name, temperature=self.temperature)
-            chain = ConversationalRetrievalChain.from_llm(
-                llm=llm,
-                condense_question_prompt=self.CONDENSE_QUESTION_PROMPT,
-                qa_prompt=self.QA_PROMPT,
-                retriever=self.vectors.as_retriever(),
-            )
+        """
+        Start a conversational chat with a model via Langchain
+        """
+        llm = ChatOpenAI(model_name=self.model_name, temperature=self.temperature)
+        chain = ConversationalRetrievalChain.from_llm(
+            llm=llm,
+            condense_question_prompt=self.CONDENSE_QUESTION_PROMPT,
+            qa_prompt=self.QA_PROMPT,
+            retriever=self.vectors.as_retriever(),
+        )
 
-            chain_input = {"question": query, "chat_history": st.session_state["history"]}
-            result = chain(chain_input)
+        chain_input = {"question": query, "chat_history": st.session_state["history"]}
+        result = chain(chain_input)
 
-            st.session_state["history"].append((query, result["answer"]))
-            #count_tokens_chain(chain, chain_input)
-            return result["answer"]
+        st.session_state["history"].append((query, result["answer"]))
+        count_tokens_chain(chain, chain_input)
+        return result["answer"]
 
+
+def count_tokens_chain(chain, query):
+    with get_openai_callback() as cb:
+        result = chain.run(query)
+        st.write(f'###### Tokens used in this conversation : {cb.total_tokens} tokens')
+    return result 
 
     
     
--- src/modules/embedder.py
+++ src/modules/embedder.py
@@ -5,8 +5,9 @@ from langchain.document_loaders.csv_loader import CSVLoader
 from langchain.vectorstores import FAISS
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.document_loaders import PyPDFLoader
-import streamlit as st
+
 class Embedder:
+
     def __init__(self):
         self.PATH = "embeddings"
         self.createEmbeddingsDir()
@@ -22,32 +23,29 @@ class Embedder:
         """
         Stores document embeddings using Langchain and FAISS
         """
-        # Write the uploaded file to a temporary file
         with tempfile.NamedTemporaryFile(mode="wb", delete=False) as tmp_file:
             tmp_file.write(file)
             tmp_file_path = tmp_file.name
             
-        # Load the data from the file using Langchain
         def get_file_extension(uploaded_file):
             file_extension =  os.path.splitext(uploaded_file)[1].lower()
             if file_extension not in [".csv", ".pdf"]:
                 raise ValueError("Unsupported file type. Only CSV and PDF files are allowed.")
+            
             return file_extension
         
         file_extension = get_file_extension(original_filename)
+
         if file_extension == ".csv":
             loader = CSVLoader(file_path=tmp_file_path, encoding="utf-8")
             data = loader.load_and_split()
+
         elif file_extension == ".pdf":
             loader = PyPDFLoader(file_path=tmp_file_path)  
             data = loader.load_and_split()
             
-        
-        
-        # Create an embeddings object using Langchain
         embeddings = OpenAIEmbeddings()
 
-        # Store the embeddings vectors using FAISS
         vectors = FAISS.from_documents(data, embeddings)
         os.remove(tmp_file_path)
 
@@ -55,13 +53,12 @@ class Embedder:
         with open(f"{self.PATH}/{original_filename}.pkl", "wb") as f:
             pickle.dump(vectors, f)
         print(data)
+        
     def getDocEmbeds(self, file, original_filename):
         """
         Retrieves document embeddings
         """
-        # Check if embeddings vectors have already been stored in a pickle file
         if not os.path.isfile(f"{self.PATH}/{original_filename}.pkl"):
-            # If not, store the vectors using the storeDocEmbeds function
             self.storeDocEmbeds(file, original_filename)
 
         # Load the vectors from the pickle file
