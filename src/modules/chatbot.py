d0cb3d7d0fad2bd77d0a8b9774c7bbe95f32c82c
Yvann <97234242+yvann-hub@users.noreply.github.com>
2023-05-08 22:15:05 +0200

    restructure and split in function csvAgent

--- src/modules/chatbot.py
+++ src/modules/chatbot.py
@@ -11,13 +11,6 @@ class Chatbot:
         self.temperature = temperature
         self.vectors = vectors
 
-    _template = """Given the following conversation and a follow-up question, rephrase the follow-up question to be a standalone question.
-        Chat History:
-        {chat_history}
-        Follow-up entry: {question}
-        Standalone question:"""
-    CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
-
     qa_template = """You are a friendly conversational assistant named Robby, designed to answer questions and chat with the user from a contextual file.
         You receive data from a user's file and a question, you must help the user find the information they need. 
         Your answers must be user-friendly and respond to the user in the language they speak to you.
@@ -25,6 +18,7 @@ class Chatbot:
         =========
         context: {context}
         ======="""
+    
     QA_PROMPT = PromptTemplate(template=qa_template, input_variables=["question", "context"])
 
     def conversational_chat(self, query):
@@ -44,6 +38,7 @@ class Chatbot:
         st.session_state["history"].append((query, result["answer"]))
         #count_tokens_chain(chain, chain_input)
         return result["answer"]
+
 
 def count_tokens_chain(chain, query):
     with get_openai_callback() as cb:
--- src/pages/2_📊 Robby-Sheet (beta).py
+++ src/pages/2_📊 Robby-Sheet (beta).py
@@ -1,14 +1,30 @@
-from modules.tabulars_tools import handle_csv_agent, handle_pandas_ai 
 import os
+import importlib
+import sys
+import pandas as pd
 import streamlit as st
 from dotenv import load_dotenv
-
+from io import StringIO, BytesIO
+from modules.robby_sheet.table_tool import CsvAgent, handle_pandas_ai
 from modules.layout import Layout
 from modules.utils import Utilities
 from modules.sidebar import Sidebar
 
+def reload_module(module_name):
+    """For update changes
+    made to modules in localhost (press r)"""
+
+    if module_name in sys.modules:
+        importlib.reload(sys.modules[module_name])
+    return sys.modules[module_name]
+
+table_tool_module = reload_module('modules.robby_sheet.table_tool')
+layout_module = reload_module('modules.layout')
+utils_module = reload_module('modules.utils')
+sidebar_module = reload_module('modules.sidebar')
+
+
 st.set_page_config(layout="wide", page_icon="💬", page_title="Robby | Chat-Bot 🤖")
-load_dotenv()
 
 layout, sidebar, utils = Layout(), Sidebar(), Utilities()
 
@@ -16,18 +32,34 @@ layout.show_header("CSV")
 
 user_api_key = utils.load_api_key()
 os.environ["OPENAI_API_KEY"] = user_api_key
+
+
 if not user_api_key:
     layout.show_api_key_missing()
-else:
 
+else:
 
     uploaded_file = utils.handle_upload(["csv"])
 
     if uploaded_file:
-        # Configure the sidebar
+
         sidebar.show_options()
 
-        handle_csv_agent(uploaded_file)
+        uploaded_file_content = BytesIO(uploaded_file.getvalue())
+        df = pd.read_csv(uploaded_file_content)
+        
+        if "chat_history" not in st.session_state:
+            st.session_state["chat_history"] = []
+        csv_agent = CsvAgent(df)
+        query = st.text_input("Ask CSV Agent :", value="", type="default",  placeholder="e-g : How many rows in my file ?")
+
+        if query :
+            result, captured_output = csv_agent.get_agent_response(query)
+            cleaned_thoughts = csv_agent.process_agent_thoughts(captured_output)
+            csv_agent.display_agent_thoughts(cleaned_thoughts)
+            csv_agent.update_chat_history(query, result)
+            csv_agent.display_chat_history()
+
 
         handle_pandas_ai(uploaded_file)
 
