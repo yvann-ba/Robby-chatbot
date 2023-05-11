e502d4e9f4fa0abe8175de1f6f5136fe01c34a67
Yvann <97234242+yvann-hub@users.noreply.github.com>
2023-05-11 11:55:53 +0200

    Put API KEY just once on robby-chatbot
    Thanks to @Patcho72
    https://github.com/yvann-hub/Robby-chatbot/issues/15

--- src/modules/utils.py
+++ src/modules/utils.py
@@ -14,15 +14,25 @@ class Utilities:
         Loads the OpenAI API key from the .env file or 
         from the user's input and returns it
         """
+        if not hasattr(st.session_state, "api_key"):
+            st.session_state.api_key = None
+        #you can define your API key in .env directly
         if os.path.exists(".env") and os.environ.get("OPENAI_API_KEY") is not None:
             user_api_key = os.environ["OPENAI_API_KEY"]
             st.sidebar.success("API key loaded from .env", icon="🚀")
         else:
-            user_api_key = st.sidebar.text_input(
-                label="#### Your OpenAI API key 👇", placeholder="sk-...", type="password"
-            )
+            if st.session_state.api_key is not None:
+                user_api_key = st.session_state.api_key
+                st.sidebar.success("API key loaded from previous input", icon="🚀")
+            else:
+                user_api_key = st.sidebar.text_input(
+                    label="#### Your OpenAI API key 👇", placeholder="sk-...", type="password"
+                )
+                if user_api_key:
+                    st.session_state.api_key = user_api_key
 
         return user_api_key
+
     
     @staticmethod
     def handle_upload(file_types):
