d8d6bd5e85c006688f44e14b1a072ecd7ec415aa
puaqieshang <qieshang@gmail.com>
2023-10-23 09:42:35 +1100

    changed main body text

--- src/Home.py
+++ src/Home.py
@@ -30,8 +30,8 @@ st.markdown("---")
 st.markdown(
     """ 
     <h5 style='text-align:center;'>Meet Bob, your intelligent payroll compliance assistant. 
-    Bob's mission is to ensure payroll compliance by comparing actual employee pay against 
-    predicted pay. Any discrepancies are cross-checked with the NSW Fairwork legislation. 🧠</h5>
+    Bob's mission is to ensure payroll compliance by comparing actual employee salary against 
+    expected salary. Any discrepancies are cross-checked with the NSW Fairwork legislation. 🧠</h5>
     """,
     unsafe_allow_html=True)
 st.markdown("---")
@@ -40,7 +40,7 @@ st.markdown("---")
 #Robby's Pages
 st.subheader("🚀 What Are We Solving?")
 st.write("""
-- **Problem**: Large enterprises like 7-Eleven have been fined millions of dollars for underpaying their employees, a practice also known as wage theft. This not only leads to significant financial losses but also stains their reputation.
+- **Problem**: Large enterprises like 7-Eleven have been fined millions of dollars for underpaying their employees. Whether intentional or not, this has not only lead to massive fines, significant financial losses as a result of poor reputation, and low morale among employees.
 - **How it works**: Bob, powered by Langchain, uses OpenAI's GPT-3 model to chat on tabular data (CSV). It extracts the most up-to-date legislation from NSW Fairwork website to check for compliance. The entire application is built using Streamlit.
 - **Features**: Bob supports CSV format as employee payroll dataset.
 """)
